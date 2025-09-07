6382d04b8ff602f5150cf270ff7bf5cff44715a5
Nilayam jiwtode <jiwtodenilayam@gmail.com>
2025-09-07 18:17:28 +0530

    day3

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+//cnd link na use karke ham apne project me react and react-dom ko use karna chahte hai isliye node_modules folder se ham usse import karenge
+import React from "react";
+import ReactDOM from "react-dom/client"///client hame react-dom ki latest version genga
+
 const element = React.createElement("h1",{id:"first", className:"first", style:{backgroundColor:"black",color:"red",fontFamily:"cursive"}},"hello coder army");
-const element2 = React.createElement("h2",{id:"second", className:"first", style:{backgroundColor:"blue",color:"white",fontFamily:"cursive"}},"maje me raho");
+const element2 = React.createElement("h2",{id:"second", className:"second", style:{backgroundColor:"blue",color:"white",fontFamily:"cursive"}},"maje me raho");
 //{}=> ye hota hai properties dene ke liye
 //render issue solve karne ke liye ham ak div banayege and usme sab element dal denge as childern
-const div = React.createElement("div",{},[element,element2])
+const div1 = React.createElement("div",{},[element,element2])
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"))
 
@@ -12,4 +16,4 @@ const reactRoot = ReactDOM.createRoot(document.getElementById("root"))
 //matlb element2 dikhega par element gayab ho jayega 
 
 //issue solve
-reactRoot.render(div);
+reactRoot.render(div1);
